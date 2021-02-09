72bfe69167ef9c90c5742afb9a1764928b4207d3
Junior Almeida <chiisaryuu@gmail.com>
2021-02-09 09:46:25 -0300

    Criando repository e service para Categoria

--- src/main/java/com/estudo/cursomc/CursomcApplication.java
+++ src/main/java/com/estudo/cursomc/CursomcApplication.java
@@ -1,7 +1,12 @@
 package com.estudo.cursomc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.estudo.cursomc.domain.Categoria;
 
 @SpringBootApplication
 public class CursomcApplication {
@@ -10,4 +15,12 @@ public class CursomcApplication {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	/*
+	 Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+	 * */
 }
