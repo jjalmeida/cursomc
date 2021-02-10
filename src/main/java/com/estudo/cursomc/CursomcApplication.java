5856553c75d46f528022266174398ae0e2daacfb
Junior Almeida <chiisaryuu@gmail.com>
2021-02-10 08:44:45 -0300

    Cliente, TipoCliente, Telefones e Enderecos

--- src/main/java/com/estudo/cursomc/CursomcApplication.java
+++ src/main/java/com/estudo/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.estudo.cursomc.domain.Categoria;
 import com.estudo.cursomc.domain.Cidade;
+import com.estudo.cursomc.domain.Cliente;
+import com.estudo.cursomc.domain.Endereco;
 import com.estudo.cursomc.domain.Estado;
 import com.estudo.cursomc.domain.Produto;
+import com.estudo.cursomc.domain.enums.TipoCliente;
 import com.estudo.cursomc.repositories.CategoriaRepository;
 import com.estudo.cursomc.repositories.CidadeRepository;
+import com.estudo.cursomc.repositories.ClienteRepository;
+import com.estudo.cursomc.repositories.EnderecoRepository;
 import com.estudo.cursomc.repositories.EstadoRepository;
 import com.estudo.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -37,6 +48,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
@@ -55,19 +67,35 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
+		/////////////////////////////////////////////////////////////
+		
 		Estado est1 = new Estado(null,"São Paulo");
 		Estado est2 = new Estado(null,"Minas Gerais");
 		
-		Cidade c1 = new Cidade(null, "São Paulo", est1);
-		Cidade c2 = new Cidade(null, "Campinas", est1);
-		Cidade c3 = new Cidade(null, "Uberlândia", est2);
+		Cidade c1 = new Cidade(null, "Uberlândia", est2);
+		Cidade c2 = new Cidade(null, "São Paulo", est1);
+		Cidade c3 = new Cidade(null, "Campinas", est1);
 		
-		est1.getCidades().addAll(Arrays.asList(c1, c2));
-		est2.getCidades().addAll(Arrays.asList(c3));
+		est1.getCidades().addAll(Arrays.asList(c2, c3));
+		est2.getCidades().addAll(Arrays.asList(c1));
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		/////////////////////////////////////////////////////////////
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "9838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
-
+	
 }
