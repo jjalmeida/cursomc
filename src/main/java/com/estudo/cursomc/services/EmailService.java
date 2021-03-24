f94e319d72684eed2ded624e3d96bef565999c4a
Junior Almeida <chiisaryuu@gmail.com>
2021-03-24 14:44:01 -0300

    Email HTML

--- src/main/java/com/estudo/cursomc/services/EmailService.java
+++ src/main/java/com/estudo/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.estudo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.estudo.cursomc.domain.Pedido;
@@ -9,4 +11,9 @@ public interface EmailService {
 	void senderOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+
 }
